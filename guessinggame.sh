c95ab0c77900e71b534022503a6e9cf082e4c004
ArunagiriS <arunagiri.msc@gmail.com>
2020-06-20 20:00:45 +0530

    Bug fixed

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-result=false
-while [[ $result -eq true ]]
+result=0
+function validateInput {
+	if [[ $userGuessCount -eq $fileCount ]]
+	then
+		echo "Congratulation!!! $fileCount files in the current directory."
+	        result=1
+	elif [[ $userGuessCount -gt $fileCount ]]
+	then
+	        echo "Your guess is too high."
+	        result=0
+	else
+	        echo "Your guess is too low."
+	        result=0
+	fi
+}
+
+while [[ $result -eq 0 ]]
 do
 	echo "Can you guess how many files are there in this directory"
 	read userGuessCount
@@ -9,15 +24,3 @@ do
 	fileCount=$(cat fileList.txt | wc -l)
 	validateInput
 done
-function validateInput {
-	if [[ $userGuessCount -eq $fileCount ]]
-	then 
-		echo "Congratulation!!! $fileCount files in the current directory."
-		result=true 
-	elif [[ $userGuessCount -gt $fileCount ]]
-	then
-		echo "Your guess is too high."
-	else
-		echo "Your guess is too low."
-	fi
-}
